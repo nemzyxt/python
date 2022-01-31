c5559c40d6e91db0feb38181ef60b402550e36c4
nemzyxt <94122064+nemzyxt@users.noreply.github.com>
2022-01-31 17:12:56 +0300

    Add files via upload

---
+++ full_name.py
@@ -0,0 +1,7 @@
+#Author : Nemuel Wainaina
+#Program takes first and second names from the user and prints the full name back to the user
+
+fname = input("Please enter your first name : ")
+sname = input("Please enter your second name : ")
+
+print(f"Your full name is : {fname} {sname}\n")
